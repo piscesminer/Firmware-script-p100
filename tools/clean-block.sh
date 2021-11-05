96224b85866357e50ea789dc3070565eec7eacb9
Your Name <you@example.com>
2021-11-05 17:12:56 +0800

    clean script

---
+++ tools/clean-block.sh
@@ -0,0 +1,13 @@
+echo "clean block"
+sudo docker stop miner
+wait
+echo "miner stop"
+rm -rf /home/pi/hnt/miner/blockchain.db/
+wait
+rm -rf /home/pi/hnt/miner/ledger.db/
+wait
+rm -rf /home/pi/hnt/miner/state_channels.db
+wait
+sudo docker start miner
+wait 
+echo "clean success"
