1c58b28254098eed2d105beeed753ae1061698fd
Your Name <you@example.com>
2021-11-17 19:41:14 +0800

    helium emergency update

--- 0.22/EU/update.sh
+++ 0.22/EU/update.sh
@@ -1,10 +1,10 @@
 echo "Eu868 update"
 mkdir /home/pi/hnt/script/update/
 wait
-mkdir /home/pi/hnt/script/update/0.21
-echo "mkdir /home/pi/hnt/script/update/0.21"
+mkdir /home/pi/hnt/script/update/0.22
+echo "mkdir /home/pi/hnt/script/update/0.22"
 wait
-sudo wget http://pisces-firmware.sidcloud.cn/0.21/sys.config -O /home/pi/hnt/script/update/0.21/sys.config;
+sudo wget http://pisces-firmware.sidcloud.cn/0.22/sys.config -O /home/pi/hnt/script/update/0.22/sys.config;
 wait
 sudo docker stop miner
 wait
@@ -23,8 +23,8 @@ sudo docker run -d --init \
 --publish 44158:44158/tcp \
 --name miner \
 --mount type=bind,source=/home/pi/hnt/miner,target=/var/data \
---mount type=bind,source=/home/pi/hnt/script/update/0.21/sys.config,target=/config/sys.config \
-quay.io/team-helium/miner:miner-arm64_2021.11.16.0
+--mount type=bind,source=/home/pi/hnt/script/update/0.22/sys.config,target=/config/sys.config \
+quay.io/team-helium/miner:miner-amd64_2021.11.16.3_GA
 
 wait
 echo "image update success"
@@ -37,16 +37,20 @@ echo "more details update"
 # echo "update dashboard over"
 # echo "update packet forward"
 # wait
-sudo curl http://127.0.0.1:8001/api/action/PacketOff
+sudo curl http://127.0.0.1:8001/api/action/PacketOff --speed-time 5 --speed-limit 1
 wait
 echo "packet stop"
-sudo wget  http://pisces-firmware.sidcloud.cn/0.21/EU/global_conf.json -O /home/pi/hnt/paket/paket/packet_forwarder/global_conf.json
+sudo wget  http://pisces-firmware.sidcloud.cn/0.22/EU/global_conf.json -O /home/pi/hnt/paket/paket/packet_forwarder/global_conf.json
 wait
 sudo curl -o curl.log "http://127.0.0.1:8001/api/action/PacketRestart" --speed-time 5 --speed-limit 1
 echo "packet on"
-
+echo "DISTRIB_RELEASE=2021.11.16.3_GA" | sudo tee /etc/lsb_release
+wait
+sudo /home/pi/config/_build/prod/rel/gateway_config/bin/gateway_config stop
+wait
+sudo /home/pi/api/tool/startAdvertise.sh
 echo "version update"
 wait
-sudo wget http://pisces-firmware.sidcloud.cn/0.21/version -O /home/pi/api/tool/version;
+sudo wget http://pisces-firmware.sidcloud.cn/0.22/version -O /home/pi/api/tool/version;
 wait
-echo "update 0.21 success"
+echo "update 0.22 success"
