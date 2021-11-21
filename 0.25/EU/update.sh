4074868409bac4422e543e2666e838e229f16633
Yago <yagop@users.noreply.github.com>
2021-11-21 20:33:12 +0100

    Generic update.sh

--- 0.25/EU/update.sh
+++ 0.25/EU/update.sh
@@ -1,52 +1,49 @@
-echo "Eu868 update"
-mkdir /home/pi/hnt/script/update/
-wait
-mkdir /home/pi/hnt/script/update/0.25
-echo "mkdir /home/pi/hnt/script/update/0.25"
+#!/bin/bash
 
-wait
-echo "clean block firt"
-sudo docker stop miner
-wait
-echo "miner stop"
-rm -rf /home/pi/hnt/miner/blockchain.db/*
-wait
-rm -rf /home/pi/hnt/miner/ledger.db/*
-wait
-sudo docker start miner
-wait 
-echo "clean success"
+# Echoes executed command
+set -x
+# Exit with error if some command fails
+set -e
 
-wait
-sudo wget http://pisces-firmware.sidcloud.cn/0.25/sys.config -O /home/pi/hnt/script/update/0.25/sys.config;
-wait
-sudo docker stop miner
-wait
-sudo docker rm miner
-wait
-echo "miner removed"
-wait
-sudo docker run -d --init \
---ulimit nofile=64000:64000 \
---env REGION_OVERRIDE=EU868 \
- --device /dev/i2c-0 \
- --net host \
- --restart always \
- --privileged \
- -v /var/run/dbus:/var/run/dbus \
---publish 1680:1680/udp \
---publish 44158:44158/tcp \
---name miner \
---mount type=bind,source=/home/pi/hnt/miner,target=/var/data \
---mount type=bind,source=/home/pi/hnt/script/update/0.24/sys.config,target=/config/sys.config \
-quay.io/team-helium/miner:miner-arm64_2021.11.21.1_GA
+FIRMWARE_VERSION="0.25"
+FIRMWARE_CONFIG_PATH="/home/pi/hnt/script/update/$FIRMWARE_VERSION"
+MINER_DOCKER_VERSION="miner-arm64_2021.11.21.1_GA"
 
-wait
-echo "image update success"
-echo "DISTRIB_RELEASE=2021.11.21.1" | sudo tee /etc/lsb_release
-wait
-echo "version update"
-wait
-sudo wget http://pisces-firmware.sidcloud.cn/0.25/version -O /home/pi/api/tool/version;
-wait
-echo "update 0.25 success"
+echo "Eu868 update $FIRMWARE_VERSION"
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+mkdir -p "$FIRMWARE_CONFIG_PATH"
+
+# Download config first to avoid stopping container if fails
+# Do not write sys.config if 404 error
+curl -Lf "http://pisces-firmware.sidcloud.cn/$FIRMWARE_VERSION/sys.config" -o "$FIRMWARE_CONFIG_PATH/sys.config"
+
+# Stop miner container if already started
+docker stop miner || true 
+docker rm miner || true 
+
+echo "Cleaning blocks"
+rm -rf "/home/pi/hnt/miner/blockchain.db/*"
+rm -rf "/home/pi/hnt/miner/ledger.db/*"
+
+echo "Running $MINER_DOCKER_VERSION image"
+
+# As it runs with "host" network no need to expose ports
+docker run -d --init \
+    --ulimit nofile=64000:64000 \
+    --env REGION_OVERRIDE=EU868 \
+    --device /dev/i2c-0 \
+    --net host \
+    --restart always \
+    --privileged \
+    -v /var/run/dbus:/var/run/dbus \
+    --name miner \
+    --mount type=bind,source=/home/pi/hnt/miner,target=/var/data \
+    --mount type=bind,source="$FIRMWARE_CONFIG_PATH/sys.config",target=/config/sys.config \
+    "quay.io/team-helium/miner:$MINER_DOCKER_VERSION"
+
+echo "Container miner running"
