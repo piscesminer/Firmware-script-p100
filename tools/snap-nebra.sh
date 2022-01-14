931f8162ad2124c5d2582b93191075fd80e1c452
Your Name <you@example.com>
2022-01-14 16:11:25 +0800

    0.35

--- tools/snap-nebra.sh
+++ tools/snap-nebra.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-sudo wget https://helium-snapshots.nebra.com/snap-1177088 -O /home/pi/hnt/miner/snap/latest
+sudo wget https://helium-snapshots.nebra.com/snap-1180376 -O /home/pi/hnt/miner/snap/latest
 sudo docker exec miner miner snapshot load /var/data/snap/latest
