19e34aa97b861071c22fcd0a57e34db5446c24f8
Your Name <you@example.com>
2022-01-23 14:02:07 +0800

    new things about global.config

--- PacketForward/Config/V2/US915.sh
+++ PacketForward/Config/V2/US915.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 sudo wget http://pisces-firmware.sidcloud.cn/PacketForward/Config/V2/US915.json -O /home/pi/hnt/paket/paket/packet_forwarder/global_conf.json
+wait
+sudo wget http://pisces-firmware.sidcloud.cn/PacketForward/Config/V2/US915 -O /home/pi/api/tool/region
+wait
+echo "US915"
--- tools/snap-nebra.sh
+++ tools/snap-nebra.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-sudo wget https://helium-snapshots.nebra.com/snap-1180376 -O /home/pi/hnt/miner/snap/latest
+sudo wget https://helium-snapshots.nebra.com/snap-1194112 -O /home/pi/hnt/miner/snap/latest
 sudo docker exec miner miner snapshot load /var/data/snap/latest
