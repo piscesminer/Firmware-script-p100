8f57cadf3d1428e11a295260b886348d70b135b1
Your Name <you@example.com>
2022-02-08 11:06:41 +0800

    del dangure sh

---
+++ PacketForward/Config/V1/EU868.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo wget http://pisces-firmware.sidcloud.cn/PacketForward/Config/V1/global_conf.json.sx1250.EU868 -O /home/pi/hnt/paket/paket/packet_forwarder/global_conf.json
+wait
+sudo wget http://pisces-firmware.sidcloud.cn/PacketForward/Config/V1/EU868 -O /home/pi/api/tool/region
+wait
+echo "EU868"
--- tools/snap-nebra.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-sudo wget https://helium-snapshots.nebra.com/snap-1208773 -O /home/pi/hnt/miner/snap/latest
-sudo docker exec miner miner snapshot load /var/data/snap/latest
