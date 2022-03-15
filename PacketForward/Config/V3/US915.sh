a9afaaadb7c3b9971ea48eb6855c07594091e5f9
Your Name <you@example.com>
2022-03-15 11:44:40 +0800

    V3 packet-forward

---
+++ PacketForward/Config/V3/US915.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo wget http://pisces-firmware.sidcloud.cn/PacketForward/Config/V3/global_conf.json.sx1250.US915V3-O /home/pi/hnt/paket/paket/packet_forwarder/global_conf.json
+wait
+sudo wget http://pisces-firmware.sidcloud.cn/PacketForward/Config/V3/US915 -O /home/pi/api/tool/region
+wait
+echo "US915"
--- tools/snap-nebra.sh
+++ tools/snap-nebra.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-sudo wget https://helium-snapshots.nebra.com/snap-1258899 -O /home/pi/hnt/miner/snap/latest
+sudo wget https://helium-snapshots.nebra.com/snap-1266812 -O /home/pi/hnt/miner/snap/latest
 sudo docker exec miner miner snapshot load /var/data/snap/latest
