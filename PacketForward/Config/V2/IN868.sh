cfefd9b12d0b1a7a749b0ca26720cb639999b31a
Your Name <you@example.com>
2022-08-23 11:57:41 +0800

    add

--- PacketForward/Config/V2/IN868.sh
+++ PacketForward/Config/V2/IN868.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo wget http://pisces-firmware.sidcloud.cn/PacketForward/Config/V2/IN868.json -O /home/pi/hnt/paket/paket/packet_forwarder/global_conf.json
+sudo wget http://pisces-firmware.sidcloud.cn/PacketForward/Config/V3/IN868.json -O /home/pi/hnt/paket/paket/packet_forwarder/global_conf.json
 wait
-sudo wget http://pisces-firmware.sidcloud.cn/PacketForward/Config/V2/IN868 -O /home/pi/api/tool/region
+sudo wget http://pisces-firmware.sidcloud.cn/PacketForward/Config/V3/IN868 -O /home/pi/api/tool/region
 wait
 echo "In868"
