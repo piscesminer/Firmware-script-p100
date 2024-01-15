ae66de12f11ce949f7af73aaadc128cc0138bd1d
Your Name <you@example.com>
2024-01-15 14:17:21 +0800

    merge

---
+++ 0.63/US/update.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Echoes executed command
+# set -x
+# Exit with error if some command fails
+# set -e
+
+FIRMWARE_VERSION="0.63"
+GATEWAY_RS_PATH="/etc/helium_gateway"
+GATEWAY_REGION="US"
+GATEWAY_VERSION="v1.3.0"
+GATEWAY_FILE="helium-gateway-1.3.0-armv7-unknown-linux-musleabihf.tar.gz"
+DISTRIB_DATE="2023.10.26"
+echo "update $FIRMWARE_VERSION"
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+
+
+
+
+# echo "Cleaning blocks"
+# rm -rf "/home/pi/hnt/miner/blockchain.db/*"
+# rm -rf "/home/pi/hnt/miner/ledger.db/*"
+mkdir -p "$GATEWAY_RS_PATH/"
+echo "🍺 mkdir $GATEWAY_RS_PATH/"
+rm -rf $GATEWAY_RS_PATH/*
+echo "🍺rm -rf $GATEWAY_RS_PATH/*"
+
+# Download the gateway_rs programe
+wget "https://github.com/helium/gateway-rs/releases/download/$GATEWAY_VERSION/$GATEWAY_FILE" -P "$GATEWAY_RS_PATH/"
+wait
+# Unzip the pack
+tar -xvf "$GATEWAY_RS_PATH/$GATEWAY_FILE" -C "$GATEWAY_RS_PATH/"
+wait
+
+# Download config
+wget "https://pisces-firmware.sidcloud.cn/$FIRMWARE_VERSION/$GATEWAY_REGION/settings.toml" -O "$GATEWAY_RS_PATH/settings.toml"
+echo "🍺 fetch https://pisces-firmware.sidcloud.cn/$FIRMWARE_VERSION/$GATEWAY_REGION/settings.toml to $GATEWAY_RS_PATH/settings.toml"
+# export PATH=/root/update/:$PATH
+
+# Download the service 
+curl -Lf "https://pisces-firmware.sidcloud.cn/$FIRMWARE_VERSION/helium.service" -o "/lib/systemd/system/helium.service"
+
+# Update the init
+curl -Lf "https://pisces-firmware.sidcloud.cn/$FIRMWARE_VERSION/init.sh" -o "/home/pi/hnt/script/init.sh"
+
+# Stop miner container if already started
+    docker stop miner || true 
+    docker rm miner || true 
+
+    systemctl daemon-reload
+# Stop the service of helium
+    service helium stop 
+
+# Start up the service
+    service helium start
+
+echo "Helium_gateway running and updated"
+
+# Update the lsb_release file
+echo "DISTRIB_RELEASE=2023.10.26" | sudo tee /etc/lsb_release
+wait
+echo "version update"
+wait
+
+# Update the version file
+sudo wget http://pisces-firmware.sidcloud.cn/$FIRMWARE_VERSION/version -O /home/pi/api/tool/version;
+wait
+echo "update $FIRMWARE_VERSION success"
